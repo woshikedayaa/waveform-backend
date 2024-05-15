32d9888bd7c6b6acf1f0c0585cb3dc0dbbf89003
woshikedayaa <woshikedayaa@gmail.com>
2024-05-15 14:45:05 +0800

    fix: install.sh typo error

--- script/install.sh
+++ script/install.sh
@@ -10,7 +10,7 @@ GITHUB_RAW_URL="https://raw.githubusercontent.com/woshikedayaa/waveform-backend/
 if [ -f ${CONFIG_FILE_PATH} ];then
   mkdir -p ${CONFIG_DIR}
   echo Download:${GITHUB_RAW_URL}/config/config_full.yaml ">" ${CONFIG_FILE_PATH}
-  curl -SLo $CONFIG_FILE_PATH
+  curl -SLo ${CONFIG_FILE_PATH}/config/config_full.yaml
 fi
 
 # todo 自动安装最新的 release
