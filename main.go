3913b10c2f9469716bb077e68d2ba1cb0c2347e6
woshikedayaa <woshikedayaa@gmail.com>
2024-05-16 19:28:32 +0800

    update: main

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"github.com/woshikedayaa/waveform-backend/api"
 	"github.com/woshikedayaa/waveform-backend/config"
+	"github.com/woshikedayaa/waveform-backend/logf"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	err := config.InitConfig()
 	if err != nil {
+		panic(err)
+	}
+	err = logf.LoggerInit()
+	if err != nil {
+		panic(err)
+	}
+	router := api.InitRouter()
+
+	// start!
+	if err = router.Run(strings.Join(
+		[]string{
+			config.G().Server.Addr,
+			strconv.Itoa(config.G().Server.Port),
+		}, ":")); err != nil {
 		panic(err)
 	}
 }
